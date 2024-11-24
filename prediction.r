c648324addb4afb4f4520040d76f568519622db3
Suhail Nawaz <suhail-nawaz@outlook.com>
2024-11-24 13:05:40 +0530

    Add files via upload

---
+++ prediction.r
@@ -0,0 +1,9 @@
+install.packages("MASS")
+install.packages("caret")
+install.packages("e1071")
+install.packages("randomForest")
+
+library(MASS)
+library(caret)
+library(e1071)
+library(randomForest)
